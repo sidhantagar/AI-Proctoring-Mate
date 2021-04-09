41508ca8ec25e98501f9504fd5e3fa52f3e70ebc
deepanshu-Raj <dcode711@gmail.com>
2021-04-10 00:13:52 +0530

    Added Home,Registration & Questions apps
    - Create views.py for the created URLs.
    - Add templates, to each of the apps.
    - Collect templates online and populate static.

---
+++ Test-Mate/test_mate/Registration/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+def Register(request):
+    pass
+
+def Login(request):
+    pass
+
+def Logout(request):
+    pass
+
+def Secondary(request):
+    pass
+
+def Activate(request,uidb64,token):
+    pass
+
+def PasswordResetEmailForm(request):
+    pass
+
+def SetNewPassword(request,uidb64,token):
+    pass
+
+def NewPassword(request):
+    pass
+
+def PwdChangedConfirmation(request):
+    pass
